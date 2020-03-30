5dc5c1b40252fe0c350d8b79ca20c8ed6a7947f4
vagusX <vagusx@vagusXs-MacBook-Pro.local>
2020-03-30 23:50:01 +0800

    feat: add fa.tsx

---
+++ external/fa.tsx
@@ -0,0 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight} from '@fortawesome/pro-light-svg-icons'
+
+const MyCustomIcon = () => {
+  return <FontAwesomeIcon icon={faChevronRight} />
+}
